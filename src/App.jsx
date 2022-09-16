ae25fd9cde9175a1a4bf238e967d3ab9e858fdfc
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-16 22:26:08 +0900

    fix: インデント修正

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,42 @@ import { InCompleteTodos } from './components/IncompleteTodos';
 import { CompleteTodos } from './components/CompleteTodos';
 
 export const App = () => {
-const [todoText, setTodoText] = useState('');
-const [incompleteTodos, setIncompleteTodos] = useState([])
-const [completeTodos, setCompleteTodos] = useState([])
+    const [todoText, setTodoText] = useState('');
+    const [incompleteTodos, setIncompleteTodos] = useState([])
+    const [completeTodos, setCompleteTodos] = useState([])
 
-const onChangeTodoText = (event) => setTodoText(event.target.value)
+    const onChangeTodoText = (event) => setTodoText(event.target.value)
 
-const onClickAdd = () => {
-    if (todoText === "") return;
-    const newTodos = [...incompleteTodos, todoText];
-    setIncompleteTodos(newTodos)
-    setTodoText("")
-};
+    const onClickAdd = () => {
+        if (todoText === "") return;
+        const newTodos = [...incompleteTodos, todoText];
+        setIncompleteTodos(newTodos)
+        setTodoText("")
+    };
 
-const onClickDelete = (index) => {
-    const newTodos = [...incompleteTodos];
-    newTodos.splice(index, 1);
-    setIncompleteTodos(newTodos)
-}
+    const onClickDelete = (index) => {
+        const newTodos = [...incompleteTodos];
+        newTodos.splice(index, 1);
+        setIncompleteTodos(newTodos)
+    }
 
-const onClickComplete = (index) => {
-    const newIncompleteTodos = [...incompleteTodos];
-    newIncompleteTodos.splice(index, 1);
-    
-    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
-    setIncompleteTodos(newIncompleteTodos);
-    setCompleteTodos(newCompleteTodos)
-}
+    const onClickComplete = (index) => {
+        const newIncompleteTodos = [...incompleteTodos];
+        newIncompleteTodos.splice(index, 1);
 
-const onClickBack = (index) => {
-    const newCompleteTodos = [...completeTodos];
-    newCompleteTodos.splice(index, 1);
+        const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+        setIncompleteTodos(newIncompleteTodos);
+        setCompleteTodos(newCompleteTodos)
+    }
 
-    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
-    setCompleteTodos(newCompleteTodos);
-    setIncompleteTodos(newIncompleteTodos);
-}
+    const onClickBack = (index) => {
+        const newCompleteTodos = [...completeTodos];
+        newCompleteTodos.splice(index, 1);
+
+        const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+        setCompleteTodos(newCompleteTodos);
+        setIncompleteTodos(newIncompleteTodos);
+    }
 
     return (
         <>
