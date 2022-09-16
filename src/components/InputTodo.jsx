d9bce0af7ceac3964828b74d6fe75b8fd46a510a
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-16 22:46:41 +0900

    feat: todoの上限を5個までに設定

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -10,15 +10,16 @@ const style = {
 }
 
 export const InputTodo = (props) => {
-    const { todoText, onChange, onClick} = props;
+    const { todoText, onChange, onClick, disabled} = props;
     return (
         <div style={style}>
             <input 
                 placeholder='Todoを入力' 
                 value={todoText} 
                 onChange={onChange}
+                disabled={disabled}
             />
-            <button onClick={onClick}>追加</button>
+            <button onClick={onClick} disabled={disabled}>追加</button>
         </div>
     )
 }
